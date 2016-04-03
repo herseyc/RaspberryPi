e1eb9836a999e4ecad93d881937f556556d9d677
Hersey <herseyc@gmail.com>
2016-04-03 08:25:29 -0400

    Update deepspace.sh

--- telescope/deepspace.sh
+++ telescope/deepspace.sh
@@ -4,12 +4,14 @@
 # Use focus.sh to focus camera and telescope
 # Start streamdeepspace.sh and access stream at http://astropiaddress:8080
 #
+imagefile=/home/pi/telescope/capture/deepspace.jpg
+shutter=5000000
+iso=800
 
-#Make the directory, it will error and continue if it already exists
-mkdir /tmp/telescope
+echo $imagefile $shutter $iso
 
 #take 5 second exposure
-raspistill -w 800 -h 600 -hf -vf -ss 5000000 -ISO 100 -sh 50 -br 50 -sa -75 -o /tmp/telescope/deepspace.jpg -tl 100 -t 999999999 -th 0:0:0
+raspistill -w 800 -h 600 -hf -vf -ss $shutter -ISO $iso -sh 50 -br 50 -sa -75 -o $imagefile -tl 100 -t 999999999 -th 0:0:0
 
 # -w width of image
 # -h height of image
